cb50fce50a22e012462bc135c4cdf9cd9f3bc69c
ian <imgriff@mit.edu>
2024-10-31 11:43:50 -0400

    Batch eval esc50

--- eval_esc_50.sh
+++ eval_esc_50.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #SBATCH --job-name=eval_esc50
-#SBATCH --output=outLogs/eval_esc50_%j.out
-#SBATCH --error=outLogs/eval_esc50_%j.err
+#SBATCH --output=outLogs/eval_esc50_%A_%a.out
+#SBATCH --error=outLogs/eval_esc50_%A_%a.err
 #SBATCH --cpus-per-task=10
 #SBATCH --gpus=1
 
@@ -9,7 +9,7 @@
 #SBATCH --time=4:00:00
 #SBATCH --partition=gpu
 #SBATCH -N 1
-##SBATCH --array=1,3-7 #0-7 for current 
+#SBATCH --array=0-53 #0-53 for current 
 
 # module load cuda cudnn nccl
 
@@ -49,14 +49,14 @@ echo "Master: "$master_node" Local node: "$HOSTNAME" GPUs used: "$CUDA_VISIBLE_D
 #                                 --ckpt_path model_checkpoints/audioset_resnet50/checkpoints/epoch=4-step=37750-best_audioset_task.ckpt
 
 
-python3 lightning_scripts/make_esc_pl_model_plots.py --config_path model_configs/pilot_ssl_audioset_resnet50.yaml \
-                                   -D /tmp/igriffith -L -4 -A 4096 -R 5 -P -O -C 0.01 0.1 1 10 100 \
-                                   --model_ckpt_dir model_checkpoints \
-                                   --ckpt_path model_checkpoints/pilot_ssl_audioset_resnet50/checkpoints/epoch=9-step=37500-v1.ckpt
-
-
-# python3 lightning_scripts/make_esc_pl_model_plots.py --config_list_path eval_config_manifests/all_config_eval_list_09_2024.pkl \
+# python3 lightning_scripts/make_esc_pl_model_plots.py --config_path model_configs/pilot_ssl_audioset_resnet50.yaml \
 #                                    -D /tmp/igriffith -L -4 -A 4096 -R 5 -P -O -C 0.01 0.1 1 10 100 \
 #                                    --model_ckpt_dir model_checkpoints \
-#                                    --array_ix $SLURM_ARRAY_TASK_ID
+#                                    --ckpt_path model_checkpoints/pilot_ssl_audioset_resnet50/checkpoints/epoch=9-step=37500-v1.ckpt
+
+
+python3 lightning_scripts/make_esc_pl_model_plots.py --config_list_path eval_config_manifests/single_task_ssl_hpara_search_esc50_list_10_2024.pkl \
+                                   -D /tmp/igriffith -L -4 -A 4096 -R 5 -P -O -C 0.01 0.1 1 10 100 \
+                                   --model_ckpt_dir model_checkpoints \
+                                   --array_ix $SLURM_ARRAY_TASK_ID
 
