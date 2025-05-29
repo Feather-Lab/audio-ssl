9f8df9e4b12e99bef5b5468c39ae52ef89818ece
ian <imgriff@mit.edu>
2025-05-29 16:59:28 -0400

    test other models on word task

--- eval_jsinv3_transfer_both_tasks.sh
+++ eval_jsinv3_transfer_both_tasks.sh
@@ -28,29 +28,29 @@ echo "Master: "$master_node" Local node: "$HOSTNAME" GPUs used: "$CUDA_VISIBLE_D
 #                                    --gpus $num_gpus --num_workers $SLURM_JOB_CPUS_PER_NODE \
 #                                    --model_ckpt_dir model_checkpoints \
 #                                    --batch_size 192 \
-#                                    --layer_str 'avgpool' \
+#                                    --layer_str 'layer3' \
 #                                    --optimizer "AdamW" --lr 0.00001 \
 #                                    --task 'word' \
 #                                    --train_epochs 6 \
 #                                    --no-with_noise --no-eval_only --no-lr_scheduler --no-use_classifier_ckpt --no-time_avg_rep --with_dropout
 
-srun python3 lightning_scripts/eval_jsin_transfer_matched.py --config_path model_configs/resnet18_barlow_equivariant_lmbda_1e-2_lr_2e-1_w_invar_augment_no_avgpool_eq_lmbda_1e-02.yaml \
-                                   --gpus $num_gpus --num_workers $SLURM_JOB_CPUS_PER_NODE \
-                                   --model_ckpt_dir model_checkpoints \
-                                   --batch_size 192 \
-                                   --layer_str 'avgpool' \
-                                   --optimizer "AdamW" --lr 0.00001 \
-                                   --task 'word' \
-                                   --train_epochs 6 \
-                                   --no-with_noise --no-eval_only --no-lr_scheduler --no-use_classifier_ckpt --no-time_avg_rep --with_dropout
-
-# srun python3 lightning_scripts/eval_jsin_transfer_matched.py --config_path model_configs/barlow_dualtask_kell2018_base_Matched_blocked_batches_lmbda_1e-2_lr_2e-1_w_augment_eq_lmbda_1e-1_fixed_loss.yaml \
+# srun python3 lightning_scripts/eval_jsin_transfer_matched.py --config_path model_configs/resnet18_barlow_equivariant_lmbda_1e-2_lr_2e-1_w_invar_augment_no_avgpool_eq_lmbda_1e-02.yaml \
 #                                    --gpus $num_gpus --num_workers $SLURM_JOB_CPUS_PER_NODE \
 #                                    --model_ckpt_dir model_checkpoints \
 #                                    --batch_size 192 \
-#                                    --layer_str 'relu2' \
-#                                    --optimizer "AdamW" --lr 0.0001 \
+#                                    --layer_str 'layer3' \
+#                                    --optimizer "AdamW" --lr 0.00001 \
 #                                    --task 'word' \
 #                                    --train_epochs 6 \
-#                                    --with_noise --no-eval_only --lr_scheduler --no-use_classifier_ckpt --no-time_avg_rep
+#                                    --no-with_noise --no-eval_only --no-lr_scheduler --no-use_classifier_ckpt --no-time_avg_rep --with_dropout
+
+srun python3 lightning_scripts/eval_jsin_transfer_matched.py --config_path model_configs/barlow_dualtask_kell2018_base_Matched_blocked_batches_lmbda_1e-2_lr_2e-1_w_augment_eq_lmbda_1e-1_fixed_loss.yaml \
+                                   --gpus $num_gpus --num_workers $SLURM_JOB_CPUS_PER_NODE \
+                                   --model_ckpt_dir model_checkpoints \
+                                   --batch_size 192 \
+                                   --layer_str 'relu2' \
+                                   --optimizer "AdamW" --lr 0.0001 \
+                                   --task 'word' \
+                                   --train_epochs 6 \
+                                   --no-with_noise --no-eval_only --lr_scheduler --no-use_classifier_ckpt --no-time_avg_rep
 
